f770e496f340902936c7b9386cdd224ba8dad53a
maksul'ka <148970035+chayniki@users.noreply.github.com>
2025-06-16 21:55:40 +0300

    Update task2.py

--- лаб 2/task2.py
+++ лаб 2/task2.py
@@ -8,90 +8,66 @@ BOOKS_DATABASE = [
         "id": 2,
         "name": "test_name_2",
         "pages": 400,
-    },
+    }
 ]
 
 
+# TODO: написать класс Book
 class Book:
-    """
-    Класс для представления книги.
-    """
+    """Класс для представления книги."""
 
-    def __init__(self, id_, name, pages):
-        """
-        Инициализация экземпляра класса Book.
-        :param id_: Идентификатор книги
-        :param name: Название книги
-        :param pages: Количество страниц в книге
-        """
-
+    def __init__(self, id_: int, name: str, pages: int):
+        # Инициализируем объект книги с id, названием и количеством страниц
         self.id = id_
         self.name = name
         self.pages = pages
 
     def __str__(self):
-        """
-        Возвращает строковое представление книги.
-        """
-
+        """Возвращает строковое представление книги."""
         return f'Книга "{self.name}"'
 
     def __repr__(self):
-        """
-        Возвращает строку, представляющую объект, которую можно использовать для его создания
-        """
-
+        """Возвращает строку, позволяющую инициализировать такой же экземпляр книги."""
         return f"Book(id_={self.id}, name='{self.name}', pages={self.pages})"
-
-
+# TODO: написать класс Library
 class Library:
-    """
-    Класс для представления библиотеки.
-    """
+    """Класс для представления библиотеки."""
 
     def __init__(self, books=None):
-        """
-        Инициализирует экземпляр класса Library.
-        """
-
+        # Инициализируем библиотеку с пустым списком книг, если список не передан
         self.books = books if books is not None else []
 
     def get_next_book_id(self):
-        """
-        Возвращает следующий доступный идентификатор для добавления новой книги
-        :return: Следующий идентификатор
-        """
+        """Возвращает следующий идентификатор для добавления новой книги."""
+        # Собираем множество существующих идентификаторов книг
+        existing_ids = {book.id for book in self.books}
+        next_id = 1
+        # Ищем минимальный доступный идентификатор
+        while next_id in existing_ids:
+            next_id += 1
+        return next_id
 
-        if not self.books:
-            return 1
-        return self.books[-1].id + 1
+    def get_index_by_book_id(self, book_id):
+        """Возвращает индекс книги по ее id."""
+         # Проходим по всем книгам и ищем книгу с заданным идентификатором
+        for index, book in enumerate(self.books):
+            if book.id == book_id:
+                return index
+        # Если книга с таким id не найдена, вызываем ошибку с нужным сообщением
+        raise ValueError("Книги с запрашиваемым id не существует")
 
-        def get_index_by_book_id(self, book_id):
-            """
-            Возвращает индекс книги по её идентификатору
-            :param book_id: Идентификатор книги
-            :return: Индекс книги в списке
-            """
-            for index, book in enumerate(self.books):
-                if book.id == book_id:
-                    return index
-            raise ValueError("Книги с запрашиваемым id не существует")
+if __name__ == '__main__':
+     # Создаем пустую библиотеку для тестирования
+    empty_library = Library()  # инициализируем пустую библиотеку
+    print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
 
-    if __name__ == "__main__":
-        empty_library = Library()  # инициализируем пустую библиотеку
-        print(
-            empty_library.get_next_book_id()
-        )  # проверяем следующий id для пустой библиотеки
+     # Создаем список книг из базы данных
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    # Инициализируем библиотеку с книгами
+    library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
+    print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
 
-        list_books = [
-            Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"])
-            for book_dict in BOOKS_DATABASE
-        ]
-        library_with_books = Library(
-            books=list_books
-        )  # инициализируем библиотеку с книгами
-        print(
-            library_with_books.get_next_book_id()
-        )  # проверяем следующий id для непустой библиотеки
-
-        print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
+    # Проверяем получение индекса книги по ее id
+    print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
